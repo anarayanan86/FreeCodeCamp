153426755b198506614371263cbdeb45d263cfaa
Arjun Narayanan <anarayanan86@gmail.com>
2016-11-14 08:10:11 -0800

    Update truncateAString.js

--- BasicAlgorithmScripting/truncateAString.js
+++ BasicAlgorithmScripting/truncateAString.js
@@ -4,8 +4,8 @@ with a ... ending.
 
 Note that inserting the three dots to the end will add to the string length.
 
-However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string
-length in determining the truncated string. */
+However, if the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the
+string length in determining the truncated string. */
 
 function truncateString(str, num) {
   // Clear out that junk in your trunk
